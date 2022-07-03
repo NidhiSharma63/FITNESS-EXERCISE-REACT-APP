d3f89a3cda4a07e04b607ff2c895ed55cc296a09
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-07-03 10:03:39 +0530

    components adde

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route,Routes} from 'react-router-dom';
+import {Box} from '@mui/material';
+
+import './App.css'
+import Navbar from './components/Navbar.jsx';
+import Footer from './components/Footer'
+import Home from './Pages/Home.jsx';
+import ExerciseDetails from './Pages/ExerciseDetails.jsx';
+
+
+import './App.jsx';
+function App() {
+  return (
+    <Router>
+      <Box width="400px" sx={{width:{xl:'1488px'}}} m='auto'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/exercise/:id' element={<ExerciseDetails/>}/>
+      </Routes>
+      <Footer/>
+      </Box>
+    </Router>
+  )
+}
+
+export default App;

af5cb6ec31b58e1caacdd48f12f5dbc432d3c136
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-07-05 20:45:23 +0530

    fontWeight removed in exerciseVideo.js

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -2,14 +2,14 @@ import React from 'react';
 import useStore from '../store';
 import { Typography, Box, Stack } from '@mui/material';
 const ExerciseVideos = () => {
-  const { exerciseVideo } = useStore();
-  // if(!exerciseVideo.length) return null;
+  const { exerciseVideo,ClickedExercise } = useStore();
+  if(!exerciseVideo.length) return null;
   console.log(exerciseVideo);
   return (
     // <>this is video</>
     <Box sx={{ marginTop: { lg: '203px', xs: '20px' } }} p="20px">
-      <Typography sx={{ fontSize: { lg: '44px', xs: '25px' } }} fontWeight={700} color="#000" mb="33px">
-        Watch <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>{}</span> exercise videos
+      <Typography sx={{ fontSize: { lg: '44px', xs: '25px' } }}  color="#000" mb="33px">
+        Watch <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>{ClickedExercise}</span> exercise videos
       </Typography>
       <Stack sx={{ flexDirection: { lg: 'row' }, gap: { lg: '110px', xs: '0px' } }} justifyContent="flex-start" flexWrap="wrap" alignItems="center">
         {exerciseVideo?.slice(0, 4)?.map((item, index) => (
@@ -22,7 +22,7 @@ const ExerciseVideos = () => {
           >
             <img style={{ borderTopLeftRadius: '20px' }} src={item.video.thumbnails[0].url} alt={item.video.title} />
             <Box>
-              <Typography sx={{ fontSize: { lg: '28px', xs: '18px' } }} fontWeight={600} color="#000">
+              <Typography sx={{ fontSize: { lg: '28px', xs: '18px' } }} color="#000">
                 {item.video.title}
               </Typography>
               <Typography fontSize="14px" color="#000">

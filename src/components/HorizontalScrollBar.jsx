0fbbbf252c7d927f638fef27ef6a096fa9ad99fc
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-07-06 10:14:26 +0530

    arrow removed

--- src/components/HorizontalScrollBar.jsx
+++ src/components/HorizontalScrollBar.jsx
@@ -1,41 +1,15 @@
-import React, { useContext } from 'react';
-import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import React from 'react';
+import { ScrollMenu} from 'react-horizontal-scrolling-menu';
 import { Box, Typography,Stack } from '@mui/material';
-import RightArrowIcon from '../assets/icon/right-arrow.png';
-import LeftArrowIcon from '../assets/icon/left-arrow.png';
 import Icon from '../assets/icon/gym.png';
 import BodyPart from './BodyParts';
-
-
-const LeftArrow = () => {
-    const { scrollPrev } = useContext(VisibilityContext);
-  
-    return (
-      <Typography onClick={() => scrollPrev()} className="right-arrow">
-        <img src={LeftArrowIcon} alt="right-arrow" />
-      </Typography>
-    );
-  };
-  
-  const RightArrow = () => {
-    const { scrollNext } = useContext(VisibilityContext);
-  
-    return (
-      <Typography onClick={() => scrollNext()} className="left-arrow">
-        <img src={RightArrowIcon} alt="right-arrow" />
-      </Typography>
-    );
-  };
-
 
 
 function HorizontalScrollBar({Array}) {
   return (
     <div style={{width:'100%',overflow:'auto'}}>
-      <ScrollMenu  
-      LeftArrow={LeftArrow} 
-      RightArrow={RightArrow} 
-      itemClassName="hrizontal" >
+      <ScrollMenu   
+        itemClassName="hrizontal" >
         {
             Array.length>0 &&
             Array.map((item)=>(

8951549828f5968fbc8b2659b7900eddcf6817a2
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-07-06 08:55:11 +0530

    meadiquery done for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,30 +9,42 @@ import CopyrightIcon from '@mui/icons-material/Copyright';
 
 const Footer = () => (
   <Box mt="80px" bgcolor="#FFF3F4" p='20px 10px'>
-    <Box sx={{border:'1px solid red'}} display='flex' justifyContent='space-between'>
+    <Box 
+      sx={{flexDirection:{sm:'row',xs:'column'}}} 
+      display='flex' 
+      alignItems='center'
+      justifyContent='space-between'>
       <Stack display='flex' justifyContent='space-around'>
-        <img src={Logo} alt="logo" style={{ width: '200px', height: '41px' }}/>
-        <Box>
+        <img src={Logo} alt="logo" style={{ width: '180px', height: '41px' }}/>
+        <Box sx={{mt:{xs:'10px',sm:'0px'}}}>
           <Typography><CopyrightIcon fontSize='10px'/>2024.All Rights Reserved.</Typography>
-          <Typography sx={{display:{md:'none',sm:'flex',xs:'none'}}}>Terms & Conditions | privacy policy</Typography>
+          <Typography 
+           sx={{display:{md:'none',sm:'flex',xs:'none'}}}
+          >Terms & Conditions | privacy policy</Typography>
         </Box>
-
       </Stack>
-      <Box display='flex' sx={{flexDirection:{sm:'column',md:'row'}}}>
+      <Box 
+        display='flex' 
+        sx={{flexDirection:{sm:'column',md:'row'},
+        mt:{xs:'20px',sm:'0px'}}}>
         <Typography 
-          sx={{alignSelf:{sm:'flex-start',md:'center'}}}
+          sx={{alignSelf:{sm:'flex-start',md:'center',xs:'center'}}}
           fontSize="22px"  
           fontFamily="Alegreya" 
           color="#3A1212" 
           textTransform="capitalize">Contact us:</Typography>
         <Stack  pl='10px' spacing={.5}>
           <Box component='span'> <TwitterIcon/><InstagramIcon/></Box>
-          <Typography fontSize="20px"  fontFamily="Alegreya" color="#3A1212" textTransform="capitalize"><EmailIcon/>:goldsgym@gmail.com</Typography>
-          <Typography fontSize="20px"  fontFamily="Alegreya" color="#3A1212" textTransform="capitalize"><LocalPhoneIcon/>:+1-844-867-5309</Typography>
+          <Typography fontSize="20px"  fontFamily="Alegreya" color="#3A1212"><EmailIcon/>:goldsgym@gmail.com</Typography>
+          <Typography fontSize="20px"  fontFamily="Alegreya" color="#3A1212"><LocalPhoneIcon/>:+1-844-867-5309</Typography>
         </Stack>
       </Box>
       <Stack 
-       sx={{display:{md:'flex',sm:'none'},alignItems:'center',justifyContent:'center', flexWrap:'wrap'}}
+       sx={{display:{md:'flex',sm:'none'},
+       mt:{xs:'20px',sm:'0px'},
+       alignItems:'center',
+       justifyContent:'center', 
+       flexWrap:'wrap'}}
      ><Typography >Terms & Conditions | privacy policy</Typography></Stack>
     </Box>
   </Box>
